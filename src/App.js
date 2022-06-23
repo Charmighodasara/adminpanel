f958ef0b2b69653a4ed93da2051380937cc61022
Charmighodasara <charmighodasara9@gmail.com>
2022-06-23 23:02:58 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { Route, Switch } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Medicines from "./containers/Medicines/Medicines";
-
+import Patients from "./containers/patients/Patients";
 
 function App() {
   return (
     <>
       <Layout>
         <Switch>
-          <Route to={'/medicines'} exact component={Medicines}></Route>
-          <Route to={'/patients'} exact component={Medicines}></Route>
+          <Route path={'/medicines'} exact component={Medicines}></Route>
+          <Route path={'/patients'} exact component={Patients}></Route>
         </Switch>
       </Layout>
     </>
