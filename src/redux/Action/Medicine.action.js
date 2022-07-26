cea1f45e2af587d2ec99d31b22f63edc60307726
Charmighodasara <charmighodasara9@gmail.com>
2022-07-26 15:51:52 +0530

    changes

--- src/redux/Action/Medicine.action.js
+++ src/redux/Action/Medicine.action.js
@@ -1,8 +1,13 @@
 import  * as Actiontypes from '../ActionType'
 
 export const getMedicines = () => (dispatch) => {
-    fetch('http://localhost:3004/medicines')
-        .then((response) => response.json())
-        .then((data) => dispatch({type : Actiontypes.MEDICINE_GETDATA, payload : data} ))
-        .catch((error)=> console.log(error));   
+    try {
+        fetch('http://localhost:3004/medicines')
+            .then((response) => response.json())
+            .then((data) => dispatch({type : Actiontypes.MEDICINE_GETDATA, payload : data} ))
+            .catch((error)=> console.log(error));   
+        
+    } catch (error) {
+        console.log(error);
+    }
 }
