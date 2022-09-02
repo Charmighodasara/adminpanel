d8d88543eac59043b7e82030deac863e567612c5
Charmighodasara <charmighodasara9@gmail.com>
2022-09-02 14:54:46 +0530

    changes

--- src/redux/Action/Doctore.action.js
+++ src/redux/Action/Doctore.action.js
@@ -1,36 +1,20 @@
 
-import { addDoc, collection } from 'firebase/firestore'
+import { addDoc, collection, getDocs } from 'firebase/firestore'
 import { BASE_URL } from '../../Base_url/Base_url'
 import { deleteDoctorsData, getDoctorsData, postDoctorsData, putDoctorsData } from '../../Commen/apis/Doctors.apis'
 import { db } from '../../firebase'
 import * as ActionTypes from '../ActionType'
 
-export const getDoctors = () => (dispatch) => {
+export const getDoctors = () => async (dispatch) => {
     try {
-        // dispatch(loadingDoctors())
-
-        // setTimeout(function () {
-        //     getDoctorsData()
-        //         .then((data) => dispatch({ type: ActionTypes.DOCTORS_GETDATA, payload: data.data }))
-        //         .catch((error) => dispatch(errorDoctors(error.message)))
-        // fetch(BASE_URL + 'Doctors')
-        //     .then(response => {
-        //         if (response.ok) {
-        //             return response;
-        //         } else {
-        //             var error = new Error('Error ' + response.status + ': ' + response.statusText);
-        //             error.response = response;
-        //             throw error;
-        //         }
-        //     },
-        //         error => {
-        //             var errmess = new Error(error.message);
-        //             throw errmess;
-        //         })
-        //     .then((response) => response.json())
-        //     .then((data) => dispatch({ type: ActionTypes.DOCTORS_GETDATA, payload: data }))
-        //     .catch((error) => dispatch(errorDoctors(error.message)))
-        // }, 2000) 
+        const querySnapshot = await getDocs(collection(db, "Doctor"));
+        let data = []
+        querySnapshot.forEach((doc) => {
+            data.push({id:doc.id, ...doc.data()})
+            console.log(`${doc.id} => ${doc.data()}`);
+        });
+        dispatch({type: ActionTypes.DOCTORS_GETDATA , payload: data })
+        console.log(data);
 
     } catch (error) {
         dispatch(errorDoctors(error.message))
@@ -38,43 +22,13 @@ export const getDoctors = () => (dispatch) => {
 
 }
 
-export const addDoctors =  (data) => async(dispatch) => {
+export const addDoctors = (data) => async (dispatch) => {
+
     try {
         const docRef = await addDoc(collection(db, "Doctor"), data);
-          console.log("Document written with ID: ", docRef.id);
-          dispatch({type : ActionTypes.DOCTORS_ADDDATA , payload:{id:docRef.id, ...data}})
-        // postDoctorsData(data)
-        // .then((data) => { dispatch({ type: ActionTypes.DOCTORS_ADDDATA, payload: data.data }); })
-        // .catch((error) => { dispatch(errorDoctors(error.message)); });
+        console.log("Document written with ID: ", docRef.id);
+        dispatch({ type: ActionTypes.DOCTORS_ADDDATA, payload: { id: docRef.id, ...data } })
 
-
-        // fetch(BASE_URL + 'Doctors', {
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //     },
-        //     body: JSON.stringify(data),
-        // })
-        //     .then(response => {
-        //         if (response.ok) {
-        //             return response;
-        //         } else {
-        //             var error = new Error('Error ' + response.status + ': ' + response.statusText);
-        //             error.response = response;
-        //             throw error;
-        //         }
-        //     },
-        //         error => {
-        //             var errmess = new Error(error.message);
-        //             throw errmess;
-        //         })
-        //     .then((response) => response.json())
-        //     .then((data) => {
-        //         dispatch({ type: ActionTypes.DOCTORS_ADDDATA, payload: data });
-        //     })
-        //     .catch((error) => {
-        //         dispatch(errorDoctors(error.message));
-        //     });
     } catch (error) {
         dispatch(errorDoctors(error.message))
     }
